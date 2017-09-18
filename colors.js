e89b00a8dd4f161fe6805e55deb39e8f3b53f237
NikolaBorislavovHristov <nikola.b.hristov@gmail.com>
2017-09-18 15:56:26 +0300

    spaces removed from colors values

--- colors.js
+++ colors.js
@@ -1,39 +1,39 @@
 module.exports = {
-    "color-white": "255, 255, 255, 1",
-    "color-white-80": "255, 255, 255, 0.8",
-    "color-white-55": "255, 255, 255, 0.55",
-    "color-white-30": "255, 255, 255, 0.3",
-    "color-white-15": "255, 255, 255, 0.15",
-    "color-white-10": "255, 255, 255, 0.1",
-    "color-prim": "99, 63, 126, 1", //stremio main
-    "color-prim-30": "99, 63, 126, 0.3",
-    "color-prim-50": "99, 63, 126, 0.5",
-    "color-prim-light": "177, 116, 215, 1", //stremio-light
-    "color-prim-light-50": "177, 116, 215, 0.5",
-    "color-prim-light-30": "177, 116, 215, 0.3",
-    "color-medium": "138, 90, 171, 1", //stremio medium
-    "color-medium-70": "138, 90, 171, 0.7",
-    "color-secondary": "58, 73, 125, 1", //secondary, header
-    "color-secondary-light": "76, 94, 155, 1", //search
-    "color-fb": "44, 62, 121, 1", //fb button color
-    "color-bg-main": "32, 31, 50, 1", //dark main
-    "color-bg-main-30": "32, 31, 50, 0.3",
-    "color-glass": "43, 44, 67, 1", //translucent
-    "color-glass-80": "43, 44, 67, 0.8",
-    "color-darkest": "24, 23, 38, 1",
-    "color-darkest-30": "24, 23, 38, 0.3",
-    "color-darkest-55": "24, 23, 38, 0.55",
-    "color-black": "0, 0, 0, 1",
-    "color-black-80": "0, 0, 0, 0.8",
-    "color-black-60": "0, 0, 0, 0.6",
-    "color-highlight": "193, 178, 203, 1",
-    "color-accent": "140, 163, 220, 1", //accent light
-    "color-accent-70": "140, 163, 220, 0.7",
-    "color-accent-30": "140, 163, 220, 0.3",
-    "color-neutral": "122, 121, 133, 1", //secondary title
-    "color-neutral-light": "168, 168, 168, 1",
-    "color-signal1": "251, 185, 25, 1",
-    "color-signal2": "251, 94, 25, 1",
-    "color-singal3": "199, 150, 44, 1",
-    "color-singal4": "25, 251, 184, 1"
+    "color-white": "255,255,255,1",
+    "color-white-80": "255,255,255,0.8",
+    "color-white-55": "255,255,255,0.55",
+    "color-white-30": "255,255,255,0.3",
+    "color-white-15": "255,255,255,0.15",
+    "color-white-10": "255,255,255,0.1",
+    "color-prim": "99,63,126,1",//stremio main
+    "color-prim-30": "99,63,126,0.3",
+    "color-prim-50": "99,63,126,0.5",
+    "color-prim-light": "177,116,215,1",//stremio-light
+    "color-prim-light-50": "177,116,215,0.5",
+    "color-prim-light-30": "177,116,215,0.3",
+    "color-medium": "138,90,171,1",//stremio medium
+    "color-medium-70": "138,90,171,0.7",
+    "color-secondary": "58,73,125,1",//secondary,header
+    "color-secondary-light": "76,94,155,1",//search
+    "color-fb": "44,62,121,1",//fb button color
+    "color-bg-main": "32,31,50,1",//dark main
+    "color-bg-main-30": "32,31,50,0.3",
+    "color-glass": "43,44,67,1",//translucent
+    "color-glass-80": "43,44,67,0.8",
+    "color-darkest": "24,23,38,1",
+    "color-darkest-30": "24,23,38,0.3",
+    "color-darkest-55": "24,23,38,0.55",
+    "color-black": "0,0,0,1",
+    "color-black-80": "0,0,0,0.8",
+    "color-black-60": "0,0,0,0.6",
+    "color-highlight": "193,178,203,1",
+    "color-accent": "140,163,220,1",//accent light
+    "color-accent-70": "140,163,220,0.7",
+    "color-accent-30": "140,163,220,0.3",
+    "color-neutral": "122,121,133,1",//secondary title
+    "color-neutral-light": "168,168,168,1",
+    "color-signal1": "251,185,25,1",
+    "color-signal2": "251,94,25,1",
+    "color-singal3": "199,150,44,1",
+    "color-singal4": "25,251,184,1"
 };
