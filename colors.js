86195e96633531d223212e60b9cc69776d57b223
NikolaBorislavovHristov <nikola.b.hristov@gmail.com>
2017-09-18 17:36:05 +0300

    colors.js export default getter function

--- colors.js
+++ colors.js
@@ -1,21 +1,12 @@
 var colors = require('./colors.json');
 
-function rgba(colorName) {
-    return 'rgba(' + colors[colorName] + ')';
-}
-
-function raw(colorName) {
+module.exports = function(colorName) {
     var colorParams = colors[colorName].split(',').map(parseFloat);
-    
+
     return {
         red: colorParams[0],
         green: colorParams[1],
         blue: colorParams[2],
         alpha: colorParams[3]
     };
-}
-
-module.exports = {
-    rgba: rgba,
-    raw: raw
 };
