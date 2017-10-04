75614c7dc30b4c463fe5c7327d7ca3159939d401
Nikola Hristov <nikola.b.hristov@gmail.com>
2017-10-04 19:13:12 +0000

    backwards compatibility with old node

--- postinstall.js
+++ postinstall.js
@@ -3,11 +3,7 @@ var colors = require('./colors.json');
 
 var STYL_FILE_PATH = './colors.styl';
 
-fs.writeFileSync(STYL_FILE_PATH, getStylContent(), function(err) {
-    if(err) {
-        throw console.error(err);
-    }
-});
+fs.writeFileSync(STYL_FILE_PATH, getStylContent());
 
 function getStylContent() {
     return Object.keys(colors)
