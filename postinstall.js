f92167f6c3d81f0554e4591840d15a8b0b4940e2
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-01-18 14:42:50 +0200

    postinstall script adapted to the new conventions

--- postinstall.js
+++ postinstall.js
@@ -1,32 +1,22 @@
-var fs = require('fs');
-var colors = require('./colors.json');
+const fs = require('fs');
+const colors = require('./colors.js');
 
-var STYL_FILE_PATH = './colors.styl';
-var ANDROID_COLOR_PATH = './android/src/main/res/values/colors.xml';
+const STYL_FILE_PATH = './colors.styl';
+const ANDROID_COLOR_PATH = './android/src/main/res/values/colors.xml';
 
 fs.writeFileSync(STYL_FILE_PATH, getStylContent());
 fs.writeFileSync(ANDROID_COLOR_PATH, getAndroidColorsContent());
 
 function getStylContent() {
-    return Object.keys(colors)
-        .map(function(colorName) {
-            return '$' + colorName + ' = rgba(' + colors[colorName] +')';
-        })
+    return Object.keys(colors.rgba)
+        .map((colorName) => `$${colorName} = ${colors.rgba[colorName]}`)
         .join('\n');
 }
 
 function getAndroidColorsContent() {
-    return Object.keys(colors)
-        .map((colorName) => {
-            const colorValues = colors[colorName].split(',');
-            const red = ('0' + parseInt(colorValues[0]).toString(16)).slice(-2);
-            const green = ('0' + parseInt(colorValues[1]).toString(16)).slice(-2);
-            const blue = ('0' + parseInt(colorValues[2]).toString(16)).slice(-2);
-            const alpha = ('0' + Math.ceil(parseFloat(colorValues[3]) * 255).toString(16)).slice(-2);
-            const sanitizedColorName = colorName.replace(/-/g, '');
-            return `\t<item name="${sanitizedColorName}" type="color">#${alpha}${red}${green}${blue}</item>`
-        })
+    return Object.keys(colors.argbHex)
+        .map((colorName) => `\t<item name="${colorName}" type="color">${colors.argbHex[colorName]}</item>`)
         .join('\n')
-        .replace(/^/,`<?xml version="1.0" encoding="utf-8"?>\n<resources>\n`)
+        .replace(/^/, `<?xml version="1.0" encoding="utf-8"?>\n<resources>\n`)
         .concat(`\n</resources>`);
 }
