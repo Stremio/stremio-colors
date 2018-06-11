6bb13e8465ae2ae42486649553d46aa23e39a756
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-06-11 16:09:07 +0300

    styl content dropped

--- postinstall.js
+++ postinstall.js
@@ -1,19 +1,11 @@
 const fs = require('fs');
 const colors = require('./colors.js');
 
-const STYL_FILE_PATH = './colors.styl';
 const LESS_FILE_PATH = './colors.less';
 const ANDROID_COLOR_PATH = './android/src/main/res/values/colors.xml';
 
-fs.writeFileSync(STYL_FILE_PATH, getStylContent());
 fs.writeFileSync(LESS_FILE_PATH, getLessContent());
 fs.writeFileSync(ANDROID_COLOR_PATH, getAndroidColorsContent());
-
-function getStylContent() {
-    return Object.keys(colors.rgba)
-        .map((colorName) => `$color${colorName} = ${colors.rgba[colorName]}`)
-        .join('\n');
-}
 
 function getLessContent() {
     return Object.keys(colors.rgba)
