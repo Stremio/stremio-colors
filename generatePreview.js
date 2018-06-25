37c1382e80837c629b5c2efd3a2eaa985c1ff3d0
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-06-25 22:46:15 +0300

    rgba in readme fixed

--- generatePreview.js
+++ generatePreview.js
@@ -10,7 +10,7 @@ ${Object.keys(colors.argbHex)
             .filter((colorName) => !colorName.includes('0'))
             .map((colorName) => {
                 const hex = colors.argbHex[colorName].substring(3);
-                const rgb = colors.rgba[colorName].substring(5, 15);
+                const rgb = colors.rgba[colorName].substring(5).slice(0, -3);
                 return `|![${colorName}](https://placehold.it/80/${hex}/000000?text=+)|${colorName}|${hex}|${rgb}|`;
             })
             .join('\n')}`;
