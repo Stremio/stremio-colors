7cc2388c1827d52f97bafc006df8f10c4c0e9200
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-12-06 12:31:32 +0200

    dashes removed from color names

--- colors.js
+++ colors.js
@@ -6,34 +6,33 @@ var colors = {
     rgba: {}
 };
 Object.keys(stremioColors).forEach(function(colorName) {
-    var sanitizedColorName = colorName.replace(/-/g, '');
     var rgbHex = stremioColors[colorName].split(',').reduce(function(result, value) {
-        return result + ('0' + Math.round(value).toString(16)).slice(-2);
+        return result + ('0' + parseInt(value).toString(16)).slice(-2);
     }, '');
 
-    colors[`${sanitizedColorName}`] = `rgba(${stremioColors[colorName]},1)`;
-    colors[`${sanitizedColorName}20`] = `rgba(${stremioColors[colorName]},0.2)`;
-    colors[`${sanitizedColorName}40`] = `rgba(${stremioColors[colorName]},0.4)`;
-    colors[`${sanitizedColorName}60`] = `rgba(${stremioColors[colorName]},0.6)`;
-    colors[`${sanitizedColorName}80`] = `rgba(${stremioColors[colorName]},0.8)`;
+    colors[`${colorName}`] = `rgba(${stremioColors[colorName]},1)`;
+    colors[`${colorName}20`] = `rgba(${stremioColors[colorName]},0.2)`;
+    colors[`${colorName}40`] = `rgba(${stremioColors[colorName]},0.4)`;
+    colors[`${colorName}60`] = `rgba(${stremioColors[colorName]},0.6)`;
+    colors[`${colorName}80`] = `rgba(${stremioColors[colorName]},0.8)`;
 
-    colors.rgba[`${sanitizedColorName}`] = colors[`${sanitizedColorName}`];
-    colors.rgba[`${sanitizedColorName}20`] = colors[`${sanitizedColorName}20`];
-    colors.rgba[`${sanitizedColorName}40`] = colors[`${sanitizedColorName}40`];
-    colors.rgba[`${sanitizedColorName}60`] = colors[`${sanitizedColorName}60`];
-    colors.rgba[`${sanitizedColorName}80`] = colors[`${sanitizedColorName}80`];
+    colors.rgba[`${colorName}`] = colors[`${colorName}`];
+    colors.rgba[`${colorName}20`] = colors[`${colorName}20`];
+    colors.rgba[`${colorName}40`] = colors[`${colorName}40`];
+    colors.rgba[`${colorName}60`] = colors[`${colorName}60`];
+    colors.rgba[`${colorName}80`] = colors[`${colorName}80`];
 
-    colors.argbHex[`${sanitizedColorName}`] = `#ff${rgbHex}`;
-    colors.argbHex[`${sanitizedColorName}20`] = `#33${rgbHex}`;
-    colors.argbHex[`${sanitizedColorName}40`] = `#66${rgbHex}`;
-    colors.argbHex[`${sanitizedColorName}60`] = `#99${rgbHex}`;
-    colors.argbHex[`${sanitizedColorName}80`] = `#cc${rgbHex}`;
+    colors.argbHex[`${colorName}`] = `#ff${rgbHex}`;
+    colors.argbHex[`${colorName}20`] = `#33${rgbHex}`;
+    colors.argbHex[`${colorName}40`] = `#66${rgbHex}`;
+    colors.argbHex[`${colorName}60`] = `#99${rgbHex}`;
+    colors.argbHex[`${colorName}80`] = `#cc${rgbHex}`;
 
-    colors.rgbaHex[`${sanitizedColorName}`] = `#${rgbHex}ff`;
-    colors.rgbaHex[`${sanitizedColorName}20`] = `#${rgbHex}33`;
-    colors.rgbaHex[`${sanitizedColorName}40`] = `#${rgbHex}66`;
-    colors.rgbaHex[`${sanitizedColorName}60`] = `#${rgbHex}99`;
-    colors.rgbaHex[`${sanitizedColorName}80`] = `#${rgbHex}cc`;
+    colors.rgbaHex[`${colorName}`] = `#${rgbHex}ff`;
+    colors.rgbaHex[`${colorName}20`] = `#${rgbHex}33`;
+    colors.rgbaHex[`${colorName}40`] = `#${rgbHex}66`;
+    colors.rgbaHex[`${colorName}60`] = `#${rgbHex}99`;
+    colors.rgbaHex[`${colorName}80`] = `#${rgbHex}cc`;
 });
 
 module.exports = colors;
