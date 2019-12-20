c45d70113130268b05d6316cf45e13152342c20c
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-12-20 17:52:51 +0200

    lightness/alpha validation improved

--- build.js
+++ build.js
@@ -12,10 +12,13 @@ const shades = Object.keys(colors)
     .map(({ name, color: [h, s, l] }) => ([
         ...Array(LIGHTNESS_SHADES_COUNT)
             .fill(null)
-            .map((_, index) => ({
-                name: `${name}-light${index + 1}`,
-                color: [h, s, Number((l + (index + 1) * LIGHTNESS_STEP).toFixed(10))]
-            }))
+            .map((_, index) => {
+                const lightness = Math.min(100, Math.max(0, Number((l + (index + 1) * LIGHTNESS_STEP).toFixed(10))));
+                return {
+                    name: `${name}-light${index + 1}`,
+                    color: [h, s, lightness]
+                };
+            })
             .reverse(),
         {
             name,
@@ -23,10 +26,13 @@ const shades = Object.keys(colors)
         },
         ...Array(LIGHTNESS_SHADES_COUNT)
             .fill(null)
-            .map((_, index) => ({
-                name: `${name}-dark${index + 1}`,
-                color: [h, s, Number((l + (index + 1) * LIGHTNESS_STEP * -1).toFixed(10))]
-            }))
+            .map((_, index) => {
+                const lightness = Math.min(100, Math.max(0, Number((l + (index + 1) * LIGHTNESS_STEP * -1).toFixed(10))));
+                return {
+                    name: `${name}-dark${index + 1}`,
+                    color: [h, s, lightness]
+                };
+            })
     ]))
     .reduce((colors, shades) => colors.concat(shades), []);
 
@@ -35,10 +41,10 @@ const shades_with_alpha = shades
         ...Array(ALPHA_SHADES_COUNT)
             .fill(null)
             .map((_, index) => {
-                const a = Number(((index + 1) * ALPHA_STEP).toFixed(10));
+                const alpha = Math.min(1, Math.max(0, Number(((index + 1) * ALPHA_STEP).toFixed(10))));
                 return {
-                    name: `${name}-${a * 100}`,
-                    color: [h, s, l, a]
+                    name: `${name}-${alpha * 100}`,
+                    color: [h, s, l, alpha]
                 };
             }),
         {
